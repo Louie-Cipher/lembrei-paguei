3c35b85f47d0d2ca4b1c7a9c93e7337b08fa9b2b
Louie-Cipher <louie.spena@outlook.com>
2023-07-28 15:08:28 -0300

    social login buttons

--- src/screens/Login/index.tsx
+++ src/screens/Login/index.tsx
@@ -1,70 +1,69 @@
-import { useState } from 'react';
-import {
-	SafeAreaView,
-	View,
-	StyleSheet,
-	Text,
-	TouchableOpacity,
-	ToastAndroid,
-} from 'react-native';
+import { SafeAreaView, View, StyleSheet, Text, TouchableOpacity } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import { propsStack } from '../../routes/Stack/Models';
-import { StatusBar } from 'expo-status-bar';
-import BaseTextInput from '../../components/BaseTextInput';
+import { StatusBar } from 'react-native';
+import { StatusBar as ExpoStatusBar } from 'expo-status-bar';
 import Colors from '../../utils/colors';
-import IsEmail from '../../utils/isEmail';
+
+import GoogleLogo from '../../../assets/google-g-logo.svg';
+import FacebookLogo from '../../../assets/facebook-logo.svg';
 
 export default () => {
 	const navigation = useNavigation<propsStack>();
 
-	const [username, setUsername] = useState('');
-	const [password, setPassword] = useState('');
-
-	const [usernameError, setUsernameError] = useState(false);
-	const [passwordError, setPasswordError] = useState(false);
-
-	const handleLogin = () => {
-		if (!username) setUsernameError(true);
-		if (!password) setPasswordError(true);
-
-		if (!username || !password)
-			return ToastAndroid.show('Preencha todos os campos', ToastAndroid.SHORT);
-
-		const isEmail = IsEmail(username);
-
-		// TODO: implement login
-		navigation.navigate('Home', { userId: '1' });
-	};
-
 	return (
 		<SafeAreaView style={styles.container}>
-			<StatusBar style='auto' />
+			<ExpoStatusBar style='auto' />
 
-			<View style={styles.buttonsContainer}>
-				<BaseTextInput
-					placeholder='nome de usuário ou email'
-					borderColor={Colors.orange}
-					error={usernameError}
-					state={username}
-					setState={setUsername}
-				/>
-				<BaseTextInput
-					placeholder='senha'
-					borderColor={Colors.orange}
-					error={passwordError}
-					type='password'
-					state={password}
-					setState={setPassword}
-				/>
+			<View style={styles.contentArea}>
+				<Text style={styles.title}>Login ou cadastro</Text>
 
-				<TouchableOpacity style={styles.button} onPress={handleLogin}>
-					<Text style={styles.buttonText}>Entrar</Text>
-				</TouchableOpacity>
+				<View style={styles.buttonsContainer}>
+					<SocialLoginButton
+						onPress={() => navigation.navigate('LoggedRoutes')}
+						text='Entrar com Google'
+						icon={<GoogleLogo width={30} height={30} />}
+					/>
+
+					<SocialLoginButton
+						onPress={() => navigation.navigate('LoggedRoutes')}
+						text='Entrar com Facebook'
+						icon={<FacebookLogo width={30} height={30} />}
+						color='#fff'
+						backgroundColor='#1977f3'
+					/>
+				</View>
 			</View>
-
 		</SafeAreaView>
 	);
 };
+
+interface SocialLoginButtonProps {
+	onPress: () => void;
+	text: string;
+	icon: JSX.Element;
+	color?: string;
+	backgroundColor?: string;
+}
+const SocialLoginButton = ({
+	onPress,
+	text,
+	icon,
+	color,
+	backgroundColor,
+}: SocialLoginButtonProps) => (
+	<TouchableOpacity
+		onPress={onPress}
+		style={[
+			styles.socialLoginButton,
+			{ backgroundColor: backgroundColor || Colors.white },
+		]}
+	>
+		<View style={styles.socialLoginIcon}>{icon}</View>
+
+		<Text style={[styles.socialLoginText, { color: color || '#000' }]}>{text}</Text>
+	</TouchableOpacity>
+);
 
 const styles = StyleSheet.create({
 	container: {
@@ -73,27 +72,39 @@ const styles = StyleSheet.create({
 		backgroundColor: Colors.white,
 		alignItems: 'center',
 	},
-	buttonsContainer: {
+	contentArea: {
 		flex: 1,
+		marginTop: StatusBar.currentHeight,
+		alignItems: 'center',
+		justifyContent: 'space-evenly',
+	},
+	title: {
+		fontSize: 30,
+		fontWeight: 'bold',
+	},
+	buttonsContainer: {
 		width: '80%',
 		justifyContent: 'center',
 		alignItems: 'center',
 	},
-	button: {
-		width: '100%',
-		padding: 15,
-		borderRadius: 10,
-		justifyContent: 'center',
+	socialLoginButton: {
+		width: '80%',
+		flexDirection: 'row',
+		paddingHorizontal: 20,
+		paddingVertical: 10,
+		marginVertical: 10,
+		borderRadius: 50,
+		shadowColor: '#000',
+		shadowRadius: 5,
+		elevation: 5,
+		justifyContent: 'space-between',
 		alignItems: 'center',
-		backgroundColor: Colors.orange,
 	},
-	buttonText: {
-		color: Colors.white,
-		fontSize: 20,
-		fontWeight: 'bold',
-	},
-	loginRedirect: {
-		fontSize: 16,
-		marginBottom: 20,
+	socialLoginIcon: {},
+	socialLoginText: {
+		flex: 1,
+		textAlign: 'center',
+		fontSize: 18,
+		fontWeight: '500',
 	},
 });
