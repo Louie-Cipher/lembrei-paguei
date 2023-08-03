3c4799df3440f16d16bfa7ee7d46b50c0816ad54
Louie-Cipher <louie.spena@outlook.com>
2023-08-03 20:20:17 -0300

    import themes and pass themeProvider

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
 import React from 'react';
-
+import { useColorScheme } from 'react-native';
+import { ThemeProvider } from 'styled-components';
+import themes from './theme';
 import Route from './routes';
 
 export default () => {
-	return <Route />;
+	const systemTheme = useColorScheme();
+
+	return (
+		<ThemeProvider theme={systemTheme === 'dark' ? themes.dark : themes.light}>
+			<Route />
+		</ThemeProvider>
+	);
 };
