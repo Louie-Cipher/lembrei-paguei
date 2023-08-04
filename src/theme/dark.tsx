91ca8e4c2d0ec3ee551bc7eb749508cb4d92cf64
Louie-Cipher <louie.spena@outlook.com>
2023-08-04 15:35:12 -0300

    new colors to themes
    orange, green, quarter tone and half tone

--- src/theme/dark.tsx
+++ src/theme/dark.tsx
@@ -1,4 +1,8 @@
 export default {
 	background: '#121212',
 	text: '#fff',
+	orange: '#FF914D',
+	green: '#82DF78',
+	quarterTone: '#1e1e1e',
+	halfTone: '#2e2e2e',
 };
--- src/theme/light.tsx
+++ src/theme/light.tsx
@@ -1,4 +1,8 @@
 export default {
-	background: '#fff',
+	background: '#ffffff',
 	text: '#000',
+	orange: '#FF914D',
+	green: '#82DF78',
+	quarterTone: '#f2f2f2',
+	halfTone: '#e5e5e5',
 };
