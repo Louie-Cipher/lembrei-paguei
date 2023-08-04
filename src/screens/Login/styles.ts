46943e60f1ffa279f434ed2e2e3ffb25689fece8
Louie-Cipher <louie.spena@outlook.com>
2023-08-04 15:36:56 -0300

    pass color properties

--- src/screens/Login/styles.ts
+++ src/screens/Login/styles.ts
@@ -2,6 +2,7 @@ import styled from 'styled-components/native';
 import { StatusBar } from 'react-native';
 
 export const Container = styled.View`
+	background-color: ${(props) => props.theme.background};
 	flex: 1;
 	justify-content: space-between;
 	align-items: center;
@@ -15,11 +16,13 @@ export const SafeArea = styled.SafeAreaView`
 `;
 
 export const Title = styled.Text`
+	color: ${(props) => props.theme.text};
 	font-size: 30px;
 	font-weight: bold;
 `;
 
 export const ButtonsContainer = styled.View`
+	flex: 1;
 	width: 80%;
 	align-items: center;
 	justify-content: center;
@@ -31,20 +34,21 @@ interface ButtonProps {
 
 export const Button = styled.TouchableOpacity<ButtonProps>`
 	background-color: ${(props) => props.backgroundColor};
-    width: 100%;
+	width: 100%;
 	flex-direction: row;
 	justify-content: space-between;
 	align-items: center;
+	margin: 5px 0;
 	padding: 10px 20px;
 	border-radius: 50px;
 	box-shadow: 0px 0px 5px #000000;
 `;
 
 interface ButtonTextProps {
-    color: string;
+	color: string;
 }
 export const SocialLoginText = styled.Text<ButtonTextProps>`
-    color: ${(props) => props.color};
+	color: ${(props) => props.color};
 	text-align: center;
 	font-size: 18px;
 	font-weight: 500;
