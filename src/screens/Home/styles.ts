75c4d8468dd6437b07fe4b8f85be5c801404dd8a
Louie-Cipher <louie.spena@outlook.com>
2023-08-04 15:37:11 -0300

    style changes

--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -17,16 +17,18 @@ export const SafeArea = styled.SafeAreaView`
 	align-items: center;
 	margin: 0;
 	padding: 0;
-	margin-top: ${StatusBar.currentHeight}px;
+	margin-top: ${StatusBar.currentHeight || 10}px;
 `;
 
 export const Title = styled.Text`
+	margin-top: 20px;
 	font-size: 30px;
 	font-weight: 700;
 	color: ${({ theme }) => theme.orange};
 `;
 
 export const Subtitle = styled.Text`
+	margin-bottom: 20px;
 	font-size: 20px;
 	font-weight: 700;
 	color: ${({ theme }) => theme.orange};
