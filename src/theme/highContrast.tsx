cdb454caa0d39a1513e0793cdfe3a99625dfa2df
Louie-Cipher <louie.spena@outlook.com>
2023-08-17 12:18:51 -0300

    create highContrast theme

---
+++ src/theme/highContrast.tsx
@@ -0,0 +1,9 @@
+export default {
+	highContrast: true,
+	background: '#000000',
+	text: '#ffffff',
+	orange: '#fc8237',
+	green: '#82DF78',
+	quarterTone: '#1e1e1e',
+	halfTone: '#2e2e2e',
+};
--- src/theme/index.ts
+++ src/theme/index.ts
@@ -1,4 +1,5 @@
 import light from './light';
 import dark from './dark';
+import highContrast from './highContrast';
 
-export default { light, dark };
+export default { light, dark, highContrast };
