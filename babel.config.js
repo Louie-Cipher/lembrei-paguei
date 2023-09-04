97ff9f86b7e97ac7494653975f23aa65d548ddc6
Louie-Cipher <louie.spena@outlook.com>
2023-09-04 19:37:03 -0300

    create module alias

--- babel.config.js
+++ babel.config.js
@@ -2,6 +2,20 @@ module.exports = function (api) {
 	api.cache(true);
 	return {
 		presets: ['babel-preset-expo'],
-		plugins: ['react-native-reanimated/plugin'],
+		plugins: [
+			'react-native-reanimated/plugin',
+			[
+				'module-resolver',
+				{
+					alias: {
+						'@': './src',
+						'@assets': './src/assets',
+						'@routes': './src/routes',
+						'@screens': './src/screens',
+						'@components': './src/components',
+					},
+				},
+			],
+		],
 	};
 };
