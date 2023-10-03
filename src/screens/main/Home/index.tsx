a02e16da3964ce75e8dedea11ac63af3d5b86bc9
Louie-Cipher <louie.spena@outlook.com>
2023-10-03 13:30:02 -0300

    use data from typeorm sqlite

--- src/screens/main/Home/index.tsx
+++ src/screens/main/Home/index.tsx
@@ -1,13 +1,68 @@
-import React from 'react';
-import LinePercentItem from '../../../components/LinePercentItem';
-import { Container, SafeArea, Title, Subtitle } from './styles';
+import { useState, useEffect } from 'react';
+import LinePercentItem from 'components/LinePercentItem';
+import { SafeArea, Title, Subtitle } from './styles';
+import CirclesBackground from 'components/CirclesBackground';
+import { GastoRepository, MetaRepository, ValoresRepository } from 'db/repositories';
+import { DB } from '@/db';
 
 export default function Home() {
+	const [currentGastos, setCurrentGastos] = useState(0);
+	const [limitGastos, setLimitGastos] = useState(0);
+
+	const [currentMetas, setCurrentMetas] = useState(0);
+	const [totalMetas, setTotalMetas] = useState(0);
+
+	const fetchTotalGastos = async () => {
+		// const last30Days = new Date().setDate(new Date().getTime() - 2_592_000_000);
+
+		// const gastos = await DB.getRepository(Gasto)
+		// 	.createQueryBuilder('gasto')
+		// 	.where('gasto.data >= :data', { data: last30Days })
+		// 	.getMany();
+
+		const gastos = await GastoRepository.find();
+
+		if (!gastos) return setCurrentGastos(0);
+
+		const total = gastos.reduce((acc, gasto): number => acc + gasto.valor, 0);
+		setCurrentGastos(total);
+
+		const valores = await new ValoresRepository().findOrCreate();
+
+		if (!valores) return setLimitGastos(0);
+
+		setLimitGastos(valores.limiteGastos);
+	};
+
+	const fetchTotalMetas = async () => {
+		const metas = await MetaRepository.find();
+
+		if (!metas) {
+			setCurrentMetas(0);
+			setTotalMetas(0);
+			return;
+		}
+
+		const current = metas.reduce((acc, meta) => acc + meta.valorAtual, 0);
+		const total = metas.reduce((acc, meta) => acc + meta.valorTotal, 0);
+
+		setCurrentMetas(current);
+		setTotalMetas(total);
+	};
+
+	const refresh = async () => {
+		if (!DB.isInitialized) await DB.initialize();
+
+		fetchTotalGastos();
+		fetchTotalMetas();
+	};
+
+	useEffect(() => {
+		refresh();
+	}, []);
+
 	return (
-		<Container
-			source={require('../../../../assets/circles-background.png')}
-			imageStyle={{ resizeMode: 'cover', opacity: 0.4 }}
-		>
+		<CirclesBackground>
 			<SafeArea>
 				<Title>Olá, {'{user}'}</Title>
 				<Subtitle>Seu resumo</Subtitle>
@@ -15,20 +70,27 @@ export default function Home() {
 				<LinePercentItem
 					title='Gastos gerais'
 					currentLabel='Atual:'
-					currentValue={500}
+					currentValue={currentGastos}
 					totalLabel='Limite:'
-					totalValue={600}
+					totalValue={limitGastos}
 				/>
 
 				<LinePercentItem
 					title='Metas de economia'
 					currentLabel='Economizou:'
-					currentValue={230.55}
+					currentValue={currentMetas}
 					totalLabel='Meta:'
-					totalValue={200}
+					totalValue={totalMetas}
 					isExceedGood
 				/>
+
+				{/* <RefreshButton
+					onPress={() => {
+						fetchTotalGastos();
+						fetchTotalMetas();
+					}}
+				/> */}
 			</SafeArea>
-		</Container>
+		</CirclesBackground>
 	);
 }
--- src/screens/main/entries/Gastos/index.tsx
+++ src/screens/main/entries/Gastos/index.tsx
@@ -1,5 +1,42 @@
-import { View } from 'react-native';
+import SimpleValueItem from 'components/SimpleValueItem';
+import CirclesBackground from 'components/CirclesBackground';
+import { SafeArea, Title } from '../styles';
+import { GastoRepository } from 'db/repositories';
+import { Gasto } from 'db/entities';
+import { useEffect, useState } from 'react';
+import { dbDateString } from 'utils/date';
 
 export default () => {
-	return <View></View>;
+	const [gastos, setGastos] = useState<Gasto[]>([]);
+
+	const fetchGastos = async () => {
+		const gastosDB = await GastoRepository.find({});
+		setGastos(gastosDB || []);
+	};
+
+	useEffect(() => {
+		fetchGastos();
+	}, []);
+	return (
+		<CirclesBackground>
+			<SafeArea>
+				<Title>Gastos</Title>
+
+				{gastos.length > 0 ? gastos.map((gasto) => (
+					<SimpleValueItem
+						key={gasto.id}
+						title={gasto.nome}
+						values={[
+							{ key: 'Categoria', value: gasto.categoria },
+							{ key: 'Valor', value: `R$ ${gasto.valor.toFixed(2)}` },
+							{
+								key: 'Data',
+								value: dbDateString(gasto.data as string),
+							},
+						]}
+					/>
+				)) : (<></>)}
+			</SafeArea>
+		</CirclesBackground>
+	);
 };
