43988fff6d436cbc9b9febaffb04035069549896
Louie-Cipher <louie.spena@outlook.com>
2023-10-03 13:32:52 -0300

    initialize typeorm expo-sqlite on root

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,20 @@
-import React from 'react';
+import { useEffect } from 'react';
 import { useColorScheme } from 'react-native';
 import { ThemeProvider } from 'styled-components';
 import themes from './theme';
 import Route from './routes';
+import { DB } from './db';
 
 export default () => {
 	const systemTheme = useColorScheme();
 
+	useEffect(() => {
+		DB.initialize();
+	}, []);
+
 	return (
 		<ThemeProvider theme={systemTheme === 'dark' ? themes.dark : themes.light}>
+			{/* <ThemeProvider theme={themes.highContrast}> */}
 			<Route />
 		</ThemeProvider>
 	);
